56a4037ddee2693c06799d14389a65aec56b77c5
vatsallsavaliya <vatsalsavaliya024@gmail.com>
2026-06-07 10:32:41 +0530

    fix: forced lowercase for navbar scss to pass Vercel build

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.scss';
+import './navbar.scss';
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
